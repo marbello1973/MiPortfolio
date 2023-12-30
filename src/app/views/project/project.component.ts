95942a24e5d95248535ba95845efa8c5814ced49
marbello1973 <mrbello1973>
2023-12-30 12:05:48 -0500

    update-web-page

--- src/app/views/project/project.component.ts
+++ src/app/views/project/project.component.ts
@@ -8,12 +8,6 @@ import { Component } from '@angular/core';
   templateUrl: './project.component.html',
   styleUrl: './project.component.css'
 })
-export class ProjectComponent {
-
-  public age: string = '+50 años, '
-  public perfil: string = 'Mi perfil se ajusta alguna vacante'
-  public perfil_lab: string = 'Desarrollador web y programdor'
-  public skills: string  = 'java - spring boot, typescrip - angular entre otras'
-
+export class ProjectComponent { 
 
 }
