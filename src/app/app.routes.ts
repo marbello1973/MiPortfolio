39e8a672eaff0086742484eb76e815140ef7c3a0
marbello1973 <mrbello1973>
2024-04-16 11:07:32 -0500

    creado componente api

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './views/home/home.component';
 import { AboutComponent } from './views/about/about.component';
 import { ProjectComponent } from './views/project/project.component';
 import { ContactComponent } from './views/contact/contact.component';
+import { ApiComponent } from './views/api/api.component';
 
 export const routes: Routes = [    
     {path: 'inicio', component: HomeComponent },
     {path: 'sobre mi', component: AboutComponent}, 
     {path: 'proyectos', component: ProjectComponent},
-    {path: 'contacto', component: ContactComponent },    
+    {path: 'contacto', component: ContactComponent },   
+    {path: 'api', component: ApiComponent},
     {path: '**', pathMatch: 'full', redirectTo: 'inicio'}  
 
 ];
--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -3,6 +3,7 @@ import {CardskillComponent} from '../../components/cardskill/cardskill.component
 import { ProjectComponent } from '../project/project.component';
 import { ContactComponent } from '../contact/contact.component';
 import { AboutComponent } from '../about/about.component';
+import { ApiComponent } from '../api/api.component';
 
 
 
@@ -14,7 +15,7 @@ import { AboutComponent } from '../about/about.component';
     ProjectComponent, 
     ContactComponent, 
     AboutComponent, 
-    
+    ApiComponent    
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
